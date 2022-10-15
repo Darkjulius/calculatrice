e86c9157120ccf7b93d7a6ceadee93797790da0b
Brice THIBAULT <70805751+Darkjulius@users.noreply.github.com>
2022-10-15 07:07:00 +0200

    ajout-fichiers
    Ajout de tous les fichiers finaux

---
+++ index.js
@@ -0,0 +1,25 @@
+window.addEventListener("DOMContentLoaded", () =>{
+    let buttons = document.querySelector(".buttons");
+    let btn = document.querySelectorAll("span");
+    let value = document.getElementById("value");
+    let toggleBtn = document.querySelector(".toggleBtn");
+    let body = document.querySelector("body");
+
+    for (let index = 0; index < btn.length; index++) {
+        btn[index].addEventListener("click", function() {
+            if(this.innerHTML == "="){
+                value.innerHTML = eval(value.innerHTML);
+            }else{
+                if(this.innerHTML == "Clear"){
+                    value.innerHTML = ""
+                }else{
+                    value.innerHTML += this.innerHTML;
+                }
+            }
+        });
+    }
+    toggleBtn.onclick = function(){
+        body.classList.toggle("dark");
+    }
+});
+
